41e4f462d4995c9e98c24cf89ca3c55fdfd9c068
Edson Toma <edsonyt15@hotmail.com>
2019-12-10 16:08:39 -0300

    move weights and cfgs downloads from trains.sh to install.sh

--- install.sh
+++ install.sh
@@ -25,3 +25,6 @@ pip install -e .
 
 mkdir bin/
 (cd bin/ && curl https://pjreddie.com/media/files/yolov2.weights -Lo yolo.weights)
+(cd darkflow/utils/ && curl "https://drive.google.com/uc?export=download&id=1-IGFd-XyIVsRWaJkgyPIvskXbMQ8j9v_" -Lo loader.py)
+(cd bin/ && curl "https://pjreddie.com/media/files/yolov2-tiny-voc.weights" -Lo tiny-yolo-voc-sign.weights)
+(cd cfg/ && curl "https://drive.google.com/uc?export=download&id=1KEZwzSZfbsI3rRibGEKse_pnTocFPe7A" -Lo tiny-yolo-voc-sign.cfg)
--- train.sh
+++ train.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-(cd darkflow/utils/ && curl "https://drive.google.com/uc?export=download&id=1-IGFd-XyIVsRWaJkgyPIvskXbMQ8j9v_" -Lo loader.py)
-(cd bin/ && curl "https://pjreddie.com/media/files/yolov2-tiny-voc.weights" -Lo tiny-yolo-voc-sign.weights)
-(cd cfg/ && curl "https://drive.google.com/uc?export=download&id=1KEZwzSZfbsI3rRibGEKse_pnTocFPe7A" -Lo tiny-yolo-voc-sign.cfg)
 curl -Lo train.zip 'https://drive.google.com/uc?export=download&id=1ATQiFtL0h9Ul4gFWL-AS1QCjC4IRtJE0'
 unzip train.zip && rm train.zip
 rm -f confirm.txt cookies.txt
