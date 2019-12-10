e23f978e10cba4a4d95b948bb55b5975436e85ab
Edson Toma <edsonyt15@hotmail.com>
2019-12-10 16:27:51 -0300

    mv labels.txt from train.sh to install.sh

--- install.sh
+++ install.sh
@@ -28,3 +28,4 @@ mkdir bin/
 (cd darkflow/utils/ && curl "https://drive.google.com/uc?export=download&id=1-IGFd-XyIVsRWaJkgyPIvskXbMQ8j9v_" -Lo loader.py)
 (cd bin/ && curl "https://pjreddie.com/media/files/yolov2-tiny-voc.weights" -Lo tiny-yolo-voc-sign.weights)
 (cd cfg/ && curl "https://drive.google.com/uc?export=download&id=1KEZwzSZfbsI3rRibGEKse_pnTocFPe7A" -Lo tiny-yolo-voc-sign.cfg)
+echo "sign" > labels.txt
--- train.sh
+++ train.sh
@@ -2,5 +2,4 @@
 curl -Lo train.zip 'https://drive.google.com/uc?export=download&id=1ATQiFtL0h9Ul4gFWL-AS1QCjC4IRtJE0'
 unzip train.zip && rm train.zip
 rm -f confirm.txt cookies.txt
-echo "sign" > labels.txt
 python flow --model cfg/tiny-yolo-voc-sign.cfg --load bin/tiny-yolo-voc-sign.weights --train --annotation ./train/annotations --dataset ./train/images --epoch 2000 --gpu 0.8
